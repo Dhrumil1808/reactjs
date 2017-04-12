218dfa2ccc4a49176b540a4b61ec92c79e0b9d36
Dhrumil1808 <dhrml.shah@gmail.com>
2017-04-12 02:12:50 -0700

    Tic tac toe in react

---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+//import logo from './logo.svg';
+import './App.css';
+import Game from './Game'
+
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
+
+
